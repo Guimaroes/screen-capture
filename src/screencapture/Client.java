fdc514b0ae802b62a51923b5085ac7c779cc51d3
gabri <gabriel.lopes2@universo.univates.br>
2021-06-24 18:53:45 -0300

    Audio

--- src/screencapture/Client.java
+++ src/screencapture/Client.java
@@ -7,11 +7,15 @@ package screencapture;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.sound.sampled.*;
 
 /**
  *
@@ -19,8 +23,20 @@ import javax.swing.JFrame;
  */
 public class Client {
     private static final int PORT = 5000;
-    private static final String IP = "177.44.248.11";
+    private static final String IP = "127.0.0.1";
+      
+    private static TargetDataLine line;
     
+    static AudioFormat getAudioFormat() {
+        float sampleRate = 16000;
+        int sampleSizeInBits = 16;
+        int channels = 2;
+        boolean signed = true;
+        boolean bigEndian = true;
+        AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits, channels, signed, bigEndian);
+        
+        return format;
+    }
     public static void main( String[] args ) {
         try {
             
@@ -28,7 +44,7 @@ public class Client {
             
             JFrame frame = new JFrame();
             
-//            frame.setResizable( true );
+//          frame.setResizable( true );
             frame.add( panel );
             frame.pack();
             frame.setVisible( true );
@@ -38,6 +54,21 @@ public class Client {
                 Socket socket = new Socket( IP, PORT );
                 
                 ObjectInputStream input = new ObjectInputStream( socket.getInputStream() );
+                
+                //AUDIO---------------------------------------------------------------
+                AudioFormat format = getAudioFormat();
+                DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            
+                if(!AudioSystem.isLineSupported(info)) {
+                    System.out.println("Line not supported");
+                    System.exit(0);
+                }
+                line = (TargetDataLine) AudioSystem.getLine(info);
+                line.open(format);
+                line.start();
+              
+                AudioInputStream in = new AudioInputStream(line);
+                //---------------------------------------------------------------------
 
                 byte[] bytes = input.readAllBytes();
 
@@ -50,9 +81,15 @@ public class Client {
                 input.close();
                 
                 socket.close();
+                
+                line.close();
+                
+                
             }
         } catch ( IOException e ) {
             System.out.println( e );
+        } catch (LineUnavailableException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
